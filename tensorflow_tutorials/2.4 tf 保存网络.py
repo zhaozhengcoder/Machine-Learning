888204c6e2d07bd8dba4b5393de1a39cf2efd8c7
赵政 <www.a821400729@qq.com>
2017-12-18 21:32:43 +0800

    保存and加载网络

---
+++ tensorflow_tutorials/2.4 tf 保存网络.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+
+# Save to file
+# remember to define the same dtype and shape when restore
+W = tf.Variable([[1,2,3],[3,4,5]], dtype=tf.float32, name='weights')
+b = tf.Variable([[1,2,3]], dtype=tf.float32, name='biases')
+
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(init)
+    save_path = saver.save(sess, "my_net/save_net.ckpt")
+    print("Save to path: ", save_path)
+
+
---
+++ tensorflow_tutorials/2.4 tf 加载网络.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+
+# restore variables
+# redefine the same shape and same type for your variables
+W = tf.Variable(np.arange(6).reshape((2, 3)), dtype=tf.float32, name="weights")
+b = tf.Variable(np.arange(3).reshape((1, 3)), dtype=tf.float32, name="biases")
+# not need init step
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    saver.restore(sess, "my_net/save_net.ckpt")
+    print("weights:", sess.run(W))
+    print("biases:", sess.run(b))
