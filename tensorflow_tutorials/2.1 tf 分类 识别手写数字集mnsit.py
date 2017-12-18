bc4eba07189a50cd67f81026049f0aea3145f651
赵政 <www.a821400729@qq.com>
2017-12-18 19:17:53 +0800

    tf

---
+++ tensorflow_tutorials/2.1 tf 分类 识别手写数字集mnsit.py
@@ -0,0 +1,58 @@
+"""
+搭建一个最简单的网络，只有输入层和输出层
+输入数据的维度是 28*28 = 784 
+输出数据的维度是 10个特征
+激活函数使用softmax
+"""
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+# number 1 to 10 data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+def add_layer(inputs, in_size, out_size, activation_function=None,):
+    # add one more layer and return the output of this layer
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b,)
+    return outputs
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+    return result
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 784]) # 28x28
+ys = tf.placeholder(tf.float32, [None, 10])
+
+# add output layer
+prediction = add_layer(xs, 784, 10,  activation_function=tf.nn.softmax)
+
+# loss函数（即最优化目标函数）选用交叉熵函数
+# 交叉熵用来衡量预测值和真实值的相似程度，如果完全相同，它们的交叉熵等于零。
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),reduction_indices=[1]))       # loss
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+
+sess = tf.Session()
+
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+    
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)   #每次只取100张图片，免得数据太多训练太慢。
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+    if i % 50 == 0:
+        # 注意，这里改成了测试集
+        print(compute_accuracy(mnist.test.images, mnist.test.labels))
