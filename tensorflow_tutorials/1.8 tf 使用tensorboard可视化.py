cbe22730eb71d148ce7968cdaa3944f48f3946cd
赵政 <www.a821400729@qq.com>
2017-12-19 16:31:55 +0800

    edited

--- tensorflow_tutorials/1.8 tf 使用tensorboard可视化.py
+++ tensorflow_tutorials/1.8 tf 使用tensorboard可视化.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+#add_layer 函数里面所有的with都是为了tensorboard添加上去的
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
     with tf.name_scope('layer'):
@@ -19,15 +20,15 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 # 使用with tf.name_scope('inputs')可以将xs和ys包含进来
 # 形成一个大的图层，图层的名字就是with tf.name_scope()方法里的参数。
 with tf.name_scope('inputs'):
-    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
-    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
+    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')  # 这个name的属性，也是为了使用tensorboard，添加上来的
+    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')  # 同上
 
 # add hidden layer
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 # add output layer
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
-# the error between prediciton and real data
+#同上，这里的with也是一样的
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))
 
@@ -36,8 +37,9 @@ with tf.name_scope('train'):
 
 sess = tf.Session()
 
-
-#在当前目录下面创建一个叫logs的目录
+# 上面的wtih或者是name都是可选的，可以选择添加，也可以选择不添加，but下面的这一行是一定要写的。
+# 这个表明了 在当前的目录下面创建以恶搞logs的文件家，然后把图的信息保存进去
+# 这样运行完这段代码之后，就会有一个logs的文件夹被创建
 if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:  # tensorflow version < 0.12
     writer = tf.train.SummaryWriter('logs/', sess.graph)
 else: # tensorflow version >= 0.12
@@ -50,6 +52,8 @@ else:
     init = tf.global_variables_initializer()
 sess.run(init)
 
+
+# 这样运行完这段代码之后，就会有一个logs的文件夹被创建，然后在logs的文件夹的目录下面 执行tensorboard 就可以可视化了
 # 执行完这段代码之后，在终端执行，下面这句：
 # $ tensorboard --logdir=logs
 
--- tensorflow_tutorials/readme.md
+++ tensorflow_tutorials/readme.md
@@ -1,9 +1,17 @@
-## tensorflow 从入门到放弃
+﻿## tensorflow 从入门到放弃
 
 
 
 
 
+### 参考资料
 
+1. 吴恩达coursera 上面的机器学习课程
+2. 吴恩达deep learning 
+3. 莫烦的tensorflow教程
+
+
+### others
 python 版本 3.5.4
+
 tensorflow 版本 1.4.0  
