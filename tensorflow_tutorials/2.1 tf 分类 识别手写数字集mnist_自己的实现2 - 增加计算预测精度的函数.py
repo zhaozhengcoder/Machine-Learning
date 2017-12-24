d706ff03066fb149317b0a007b494083dbe70475
赵政 <www.a821400729@qq.com>
2017-12-24 15:18:30 +0800

    add

--- tensorflow_tutorials/2.1 tf 分类 识别手写数字集mnist_自己的实现2 - 增加计算预测精度的函数.py
+++ tensorflow_tutorials/2.1 tf 分类 识别手写数字集mnist_自己的实现2 - 增加计算预测精度的函数.py
@@ -50,23 +50,24 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-for i in range(1000):
+for i in range(500):
     batch_xs,batch_ys =mnist.train.next_batch(100)
     sess.run(train_step,feed_dict={x_input:batch_xs , y_input:batch_ys})
 
     if (i%50==0):
         #print ("loss : ",sess.run(cross_entropy))  --> 之前这么写得，都是报错，因为没有加feed_dict 
         print ("loss : ",sess.run(cross_entropy,feed_dict={x_input:batch_xs , y_input:batch_ys}))
+        # 这个越大越好
         print ("prediction acc : ",compute_acc(mnist.test.images[:100], mnist.test.labels[:100]))
 
-"""
+
 #从测试集里面拿一些数据 试一试结果
-res=sess.run(y_pre,feed_dict={x_input : mnist.test.images[:examples_to_show]} )
+test_res=sess.run(y_pre,feed_dict={x_input : mnist.test.images[:examples_to_show]} )
 #print (res)
 #print (res.shape)  # shape : examples_to_show * 10 
 
 # 输出图片识别的结果
-print ("number is : ", sess.run(tf.argmax(res,1)))
+print ("number is : ", sess.run(tf.argmax(test_res,1)))
 
 # show 一下图片
 # 这段代码 是从别的地方copy 过来的，作用是将图片show出来
@@ -76,5 +77,5 @@ for i in range(examples_to_show):
     #a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
 plt.show()
 
-"""
+
 sess.close()
